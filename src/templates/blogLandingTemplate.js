0d2aff55e5dfd2367d82581bc35588c3a5239810
sandywyper <sandywyper@gmail.com>
2020-05-25 15:52:45 +0100

    Changed Project and Blog templates into functional components from class components because they don't use state.  Added prop of path to 404 page template

--- src/templates/blogLandingTemplate.js
+++ src/templates/blogLandingTemplate.js
@@ -3,35 +3,33 @@ import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
 import PropTypes from "prop-types"
 
-class BlogList extends React.Component {
-  render() {
-    // render navigation between blog-listing pages if there are more then one.
-    const { currentPage, numBlogPages } = this.props.pageContext
-    const isFirst = currentPage === 1
-    const isLast = currentPage === numBlogPages
-    const prevPage = currentPage - 1 === 1 ? "/" : (currentPage - 1).toString()
-    const nextPage = (currentPage + 1).toString()
-    const posts = this.props.data.allMarkdownRemark.edges
+const BlogList = props => {
+  // render navigation between blog-listing pages if there are more then one.
+  const { currentPage, numBlogPages } = props.pageContext
+  const isFirst = currentPage === 1
+  const isLast = currentPage === numBlogPages
+  const prevPage = currentPage - 1 === 1 ? "/" : (currentPage - 1).toString()
+  const nextPage = (currentPage + 1).toString()
+  const posts = props.data.allMarkdownRemark.edges
 
-    return (
-      <Layout path={this.props.path}>
-        {posts.map(({ node }) => {
-          const title = node.frontmatter.title || node.fields.slug
-          return <div key={node.fields.slug}>{title}</div>
-        })}
-        {!isFirst && (
-          <Link to={`/blog/${prevPage}`} rel="prev">
-            ← Previous Page
-          </Link>
-        )}
-        {!isLast && (
-          <Link to={`/blog/${nextPage}`} rel="next">
-            Next Page →
-          </Link>
-        )}
-      </Layout>
-    )
-  }
+  return (
+    <Layout path={props.path}>
+      {posts.map(({ node }) => {
+        const title = node.frontmatter.title || node.fields.slug
+        return <div key={node.fields.slug}>{title}</div>
+      })}
+      {!isFirst && (
+        <Link to={`/blog/${prevPage}`} rel="prev">
+          ← Previous Page
+        </Link>
+      )}
+      {!isLast && (
+        <Link to={`/blog/${nextPage}`} rel="next">
+          Next Page →
+        </Link>
+      )}
+    </Layout>
+  )
 }
 
 export const blogListQuery = graphql`
@@ -55,6 +53,7 @@ export const blogListQuery = graphql`
     }
   }
 `
+
 BlogList.propTypes = {
   pageContext: PropTypes.object.isRequired,
   path: PropTypes.string.isRequired,
--- src/templates/projectsLandingTemplate.js
+++ src/templates/projectsLandingTemplate.js
@@ -3,34 +3,33 @@ import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import PropTypes from "prop-types"
 
-class ProjectList extends React.Component {
-  render() {
-    // render navigation between blog-listing pages if there are more then one.
-    const { currentPage, numBlogPages } = this.props.pageContext
-    const isFirst = currentPage === 1
-    const isLast = currentPage === numBlogPages
-    const prevPage = currentPage - 1 === 1 ? "/" : (currentPage - 1).toString()
-    const nextPage = (currentPage + 1).toString()
-    const posts = this.props.data.allMarkdownRemark.edges
-    return (
-      <Layout path={this.props.path}>
-        {posts.map(({ node }) => {
-          const title = node.frontmatter.title || node.fields.slug
-          return <div key={node.fields.slug}>{title}</div>
-        })}
-        {!isFirst && (
-          <Link to={`/blog/${prevPage}`} rel="prev">
-            ← Previous Page
-          </Link>
-        )}
-        {!isLast && (
-          <Link to={`/blog/${nextPage}`} rel="next">
-            Next Page →
-          </Link>
-        )}
-      </Layout>
-    )
-  }
+const ProjectList = props => {
+  // render navigation between blog-listing pages if there are more then one.
+  const { currentPage, numProjectPages } = props.pageContext
+  const isFirst = currentPage === 1
+  const isLast = currentPage === numProjectPages
+  const prevPage = currentPage - 1 === 1 ? "/" : (currentPage - 1).toString()
+  const nextPage = (currentPage + 1).toString()
+  const posts = props.data.allMarkdownRemark.edges
+
+  return (
+    <Layout path={props.path}>
+      {posts.map(({ node }) => {
+        const title = node.frontmatter.title || node.fields.slug
+        return <div key={node.fields.slug}>{title}</div>
+      })}
+      {!isFirst && (
+        <Link to={`/projects/${prevPage}`} rel="prev">
+          ← Previous Page
+        </Link>
+      )}
+      {!isLast && (
+        <Link to={`/projects/${nextPage}`} rel="next">
+          Next Page →
+        </Link>
+      )}
+    </Layout>
+  )
 }
 
 export const projectListQuery = graphql`
