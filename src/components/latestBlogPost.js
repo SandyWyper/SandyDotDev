72bee59c08828e586e30df94a0785936a436bcee
sandywyper <sandywyper@gmail.com>
2020-05-27 08:08:38 +0100

    Copied latestProject to a similar latestBlogPost component

--- src/components/latestBlogPost.js
+++ src/components/latestBlogPost.js
@@ -1,7 +1,60 @@
 import React from "react"
+import { useStaticQuery, graphql, Link } from "gatsby"
+import Img from "gatsby-image"
 
 const LatestBlogPost = () => {
-  return <section>This is the latest Blog post section</section>
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark(
+        filter: { fields: { slug: { regex: "/blog/" } } }
+        sort: { fields: [frontmatter___date], order: DESC }
+        limit: 1
+      ) {
+        edges {
+          node {
+            frontmatter {
+              title
+              date(formatString: "DD/MM/YYYY")
+              tags
+              posttype
+              description
+              cover {
+                childImageSharp {
+                  fluid(maxWidth: 1500) {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
+            }
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+  const postDetails = data.allMarkdownRemark.edges[0].node.frontmatter
+  const postPath = data.allMarkdownRemark.edges[0].node.fields.slug
+  return (
+    <section style={{ border: "1px solid grey" }}>
+      <header>
+        <Link to="/blog">
+          <h3>Blog</h3>
+        </Link>
+      </header>
+      <h2>{postDetails.title}</h2>
+      <p>{postDetails.date}</p>
+      <p>{postDetails.description}</p>
+      <Img fluid={postDetails.cover.childImageSharp.fluid} />
+      <ul>
+        {postDetails.tags.map(item => {
+          return <li key={item}>{item}</li>
+        })}
+      </ul>
+      <Link to={postPath}>Read more...</Link>
+    </section>
+  )
 }
 
 export default LatestBlogPost
--- src/components/latestProject.js
+++ src/components/latestProject.js
@@ -38,10 +38,20 @@ const LatestProject = () => {
   const projectPath = data.allMarkdownRemark.edges[0].node.fields.slug
   return (
     <section style={{ border: "1px solid grey" }}>
+      <header>
+        <Link to="/projects">
+          <h3>Projects</h3>
+        </Link>
+      </header>
       <h2>{projectDetails.title}</h2>
       <p>{projectDetails.date}</p>
       <p>{projectDetails.description}</p>
       <Img fluid={projectDetails.cover.childImageSharp.fluid} />
+      <ul>
+        {projectDetails.tags.map(item => {
+          return <li key={item}>{item}</li>
+        })}
+      </ul>
       <Link to={projectPath}>Read more...</Link>
     </section>
   )
