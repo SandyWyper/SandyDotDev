42379e52bcada0a59a35ebbaa852ab851860f35f
sandywyper <sandywyper@gmail.com>
2020-06-04 11:20:47 +0100

    Componentised social links and included them into contact form and replaced instances elsewhere on the site

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
+import LinkedInLink from "../components/linkedInLink"
+import GitHubLink from "../components/gitHubLink"
 
 export default function Contact(props) {
   return (
@@ -7,7 +9,7 @@ export default function Contact(props) {
       <section className="container mx-auto">
         <h1 className="mb-6">Contact</h1>
         <form
-          className="w-full max-w-md px-4"
+          className="w-full max-w-lg px-4"
           method="post"
           name="contact"
           data-netlify="true"
@@ -75,50 +77,29 @@ export default function Contact(props) {
               <input
                 type="submit"
                 value="Send Message"
-                className="w-full px-4 py-2 font-bold text-white bg-green-600 rounded shadow hover:bg-green-600 hover:bg-opacity-75 focus:shadow-outline focus:outline-none"
+                className="w-full px-4 py-2 font-bold text-white bg-green-600 rounded shadow cursor-pointer hover:bg-green-600 hover:bg-opacity-75 focus:shadow-outline focus:outline-none"
               />
             </div>
             <div className="md:ml-2 md:w-1/3">
               <input
                 type="reset"
                 value="Reset"
-                className="w-full px-4 py-2 font-bold text-green-600 bg-gray-200 rounded shadow hover:opacity-75 focus:shadow-outline focus:outline-none"
+                className="w-full px-4 py-2 font-bold text-green-600 bg-gray-200 rounded shadow cursor-pointer hover:opacity-75 focus:shadow-outline focus:outline-none"
               />
             </div>
           </div>
         </form>
-        <ul className="">
-          <li>
-            <a
-              href="https://www.linkedin.com/in/sandy-wyper-6507611a7/"
-              className="icon fa-linkedin-square"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <span className="label">Linkedin</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.instagram.com/sandypants4life/"
-              className="icon fa-instagram"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <span className="label">Instagram</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://github.com/SandyWyper"
-              className="icon fa-github"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <span className="label">GitHub</span>
-            </a>
-          </li>
-        </ul>
+        <div className="flex w-full max-w-lg px-4">
+          <div className="md:w-1/3" />
+          <div className="flex justify-end w-full md:w-2/3">
+            <div className="w-8 ml-4 scale-hover-2">
+              <LinkedInLink />
+            </div>
+            <div className="w-8 ml-4 scale-hover-2">
+              <GitHubLink />
+            </div>
+          </div>
+        </div>
       </section>
     </Layout>
   )
