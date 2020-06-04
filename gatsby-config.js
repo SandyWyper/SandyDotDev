910bfa3906a4d4a293f2c25c99fbfe48e694d3f5
sandywyper <sandywyper@gmail.com>
2020-06-04 14:42:04 +0100

    DEbugger

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "posts",
-        path: `${__dirname}/content`,
+        path: `${__dirname}/src/content`,
       },
     },
     {
