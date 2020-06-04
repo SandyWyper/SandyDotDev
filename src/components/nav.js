019601dbfbefe0299a41163b508e9189bfb13795
sandywyper <sandywyper@gmail.com>
2020-06-04 21:00:13 +0100

    Changed the order of the nav logic

--- src/components/nav.js
+++ src/components/nav.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types"
 import HomeNav from "./homeNav"
 import TopNav from "./topNav"
 
-const Nav = props => (props.path === "/" ? <HomeNav /> : <TopNav />)
+const Nav = props => (props.path !== "/" ? <TopNav /> : <HomeNav />)
 
 Nav.propTypes = {
   path: PropTypes.string.isRequired,
