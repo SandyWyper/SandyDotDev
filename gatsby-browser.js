30a9dfe03c7c78c6e890182186ed6382ff49fd20
sandywyper <sandywyper@gmail.com>
2020-06-04 12:18:49 +0100

    Attempting to fix issue with styles not loading correctly on first paint

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,7 +6,7 @@
 
 // You can delete this file if you're not using it
 
-import "./src/styles/index.scss"
+require("./src/styles/index.scss")
 
 require("prismjs/themes/prism-tomorrow.css")
 // require("prismjs/themes/prism-solarizedlight.css")
