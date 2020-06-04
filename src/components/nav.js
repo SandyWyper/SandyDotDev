94a06a75395a475585176b7bdca501c624635d6a
sandywyper <sandywyper@gmail.com>
2020-06-04 20:37:46 +0100

    Checking nav logic

--- src/components/nav.js
+++ src/components/nav.js
@@ -3,7 +3,12 @@ import PropTypes from "prop-types"
 import HomeNav from "./homeNav"
 import TopNav from "./topNav"
 
-const Nav = props => (props.path === "/" ? <HomeNav /> : <TopNav />)
+const Nav = props =>
+  props.path === "/" || props.location.key === "initial" ? (
+    <HomeNav />
+  ) : (
+    <TopNav />
+  )
 
 Nav.propTypes = {
   path: PropTypes.string.isRequired,
