b169d3b5ce91ca090f05aa46a9f95f17bcee2916
sandywyper <sandywyper@gmail.com>
2020-06-05 12:01:29 +0100

    Changed transition ease for custom besier curve amd tidy up navs

--- src/components/homeNav.js
+++ src/components/homeNav.js
@@ -3,29 +3,27 @@ import { Link } from "gatsby"
 
 const HomeNav = () => {
   return (
-    <>
-      <nav className="home-nav this-is-home-nav">
-        <ul>
-          <li className="hidden md:block this-is-inside-the-home-nav">
-            <Link to="/projects">Projects</Link>
-          </li>
-          <li className="hidden md:block">
-            <Link to="/blog">Blog</Link>
-          </li>
-          <li className="hidden md:block">
-            <Link to="/contact">Contact</Link>
-          </li>
-          <button
-            className="hamburger hamburger--collapse md:hidden"
-            type="button"
-          >
-            <span className="hamburger-box">
-              <span className="hamburger-inner"></span>
-            </span>
-          </button>
-        </ul>
-      </nav>
-    </>
+    <nav className="home-nav">
+      <ul>
+        <li className="hidden md:block">
+          <Link to="/projects">Projects</Link>
+        </li>
+        <li className="hidden md:block">
+          <Link to="/blog">Blog</Link>
+        </li>
+        <li className="hidden md:block">
+          <Link to="/contact">Contact</Link>
+        </li>
+        <button
+          className="hamburger hamburger--collapse md:hidden"
+          type="button"
+        >
+          <span className="hamburger-box">
+            <span className="hamburger-inner"></span>
+          </span>
+        </button>
+      </ul>
+    </nav>
   )
 }
 export default HomeNav
