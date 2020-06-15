2844ff589e81746abd5e164c15bf6617c9a3fdad
sandywyper <sandywyper@gmail.com>
2020-06-15 13:06:20 +0100

    Manipulating the layout logic

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,3 +1,54 @@
+// import React from "react"
+// import { TransitionProvider, TransitionViews } from "gatsby-plugin-transitions"
+// import Footer from "../components/footer"
+// import TopNav from "../components/topNav"
+// import MobileNav from "../components/mobileNav"
+// import PropTypes from "prop-types"
+
+// const Layout = ({ pageContext, children, location }) => {
+//   return (
+//     <div className="relative overflow-hidden">
+//       <MobileNav />
+//       {pageContext.homeLayout ? (
+//         <TransitionProvider location={location}>
+//           <TransitionViews>{children}</TransitionViews>
+//         </TransitionProvider>
+//       ) : (
+//         <>
+//           <TopNav title={pageContext.navTitle} />
+//           <TransitionProvider
+//             location={location}
+//             mode="successive"
+//             enter={{
+//               opacity: 0,
+//             }}
+//             usual={{
+//               opacity: 1,
+//             }}
+//             leave={{
+//               opacity: 0,
+
+//               config: {
+//                 duration: 100,
+//               },
+//             }}
+//           >
+//             <TransitionViews>{children}</TransitionViews>
+//           </TransitionProvider>
+//         </>
+//       )}
+//       <Footer />
+//     </div>
+//   )
+// }
+
+// Layout.propTypes = {
+//   pageContext: PropTypes.object.isRequired,
+//   children: PropTypes.node.isRequired,
+//   location: PropTypes.object.isRequired,
+// }
+// export default Layout
+
 import React from "react"
 import { TransitionProvider, TransitionViews } from "gatsby-plugin-transitions"
 import Footer from "../components/footer"
@@ -6,40 +57,44 @@ import MobileNav from "../components/mobileNav"
 import PropTypes from "prop-types"
 
 const Layout = ({ pageContext, children, location }) => {
-  return (
-    <div className="relative overflow-hidden">
-      <MobileNav />
-      {pageContext.homeLayout ? (
+  if (pageContext.homeLayout) {
+    return (
+      <div className="relative overflow-hidden">
+        <MobileNav />
         <TransitionProvider location={location}>
           <TransitionViews>{children}</TransitionViews>
         </TransitionProvider>
-      ) : (
-        <>
-          <TopNav title={pageContext.navTitle} />
-          <TransitionProvider
-            location={location}
-            mode="successive"
-            enter={{
-              opacity: 0,
-            }}
-            usual={{
-              opacity: 1,
-            }}
-            leave={{
-              opacity: 0,
+        <Footer />
+      </div>
+    )
+  } else {
+    return (
+      <div className="relative overflow-hidden">
+        <MobileNav />
+        <TopNav title={pageContext.navTitle} />
+        <TransitionProvider
+          location={location}
+          mode="successive"
+          enter={{
+            opacity: 0,
+          }}
+          usual={{
+            opacity: 1,
+          }}
+          leave={{
+            opacity: 0,
 
-              config: {
-                duration: 100,
-              },
-            }}
-          >
-            <TransitionViews>{children}</TransitionViews>
-          </TransitionProvider>
-        </>
-      )}
-      <Footer />
-    </div>
-  )
+            config: {
+              duration: 100,
+            },
+          }}
+        >
+          <TransitionViews>{children}</TransitionViews>
+        </TransitionProvider>
+        <Footer />
+      </div>
+    )
+  }
 }
 
 Layout.propTypes = {
