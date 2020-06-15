98da591972f26a4d8d60adb0861f45ffb95ead83
sandywyper <sandywyper@gmail.com>
2020-06-15 12:19:38 +0100

    console logs removed from layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,52 +7,46 @@ import PropTypes from "prop-types"
 
 const Layout = ({ pageContext, children, location }) => {
   const isBrowser = typeof window !== `undefined`
-  if (isBrowser) {
-    return (
-      <div className="relative overflow-hidden">
-        {console.log("layout render")}
-        <MobileNav />
-        {pageContext.homeLayout ? (
-          <TransitionProvider location={location}>
-            <TransitionViews>
-              {console.log("home layout render")}
-              {children}
-            </TransitionViews>
-          </TransitionProvider>
-        ) : (
-          <>
-            <TopNav title={pageContext.navTitle} />
-            <TransitionProvider
-              location={location}
-              mode="successive"
-              enter={{
-                opacity: 0,
-              }}
-              usual={{
-                opacity: 1,
-              }}
-              leave={{
-                opacity: 0,
-
-                config: {
-                  duration: 100,
-                },
-              }}
-            >
-              <TransitionViews>
-                {console.log("other layout render")}
-                {children}
-              </TransitionViews>
-            </TransitionProvider>
-          </>
-        )}
-        <Footer />
-      </div>
-    )
-  }
+  // if (isBrowser) {
   return (
-    <div className="min-w-full min-h-screen border bg-custom-blue border-custom-black"></div>
+    <div className="relative overflow-hidden">
+      {console.log("layout render")}
+      <MobileNav />
+      {pageContext.homeLayout ? (
+        <TransitionProvider location={location}>
+          <TransitionViews>{children}</TransitionViews>
+        </TransitionProvider>
+      ) : (
+        <>
+          <TopNav title={pageContext.navTitle} />
+          <TransitionProvider
+            location={location}
+            mode="successive"
+            enter={{
+              opacity: 0,
+            }}
+            usual={{
+              opacity: 1,
+            }}
+            leave={{
+              opacity: 0,
+
+              config: {
+                duration: 100,
+              },
+            }}
+          >
+            <TransitionViews>{children}</TransitionViews>
+          </TransitionProvider>
+        </>
+      )}
+      <Footer />
+    </div>
   )
+  // }
+  // return (
+  //   <div className="min-w-full min-h-screen border bg-custom-blue border-custom-black"></div>
+  // )
 }
 
 Layout.propTypes = {
