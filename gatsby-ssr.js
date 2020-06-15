8affd322a23c21b0fd5b41969a2b0dcca7ef5dff
sandywyper <sandywyper@gmail.com>
2020-06-15 11:57:08 +0100

    Tried puting the contents of Browser.js into ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -6,6 +6,14 @@
 
 // You can delete this file if you're not using it
 
-// import "./src/styles/index.scss"
+// styles for code blocks
+require("prismjs/themes/prism-tomorrow.css")
+// styles for the site that get purged
+require("./src/styles/index.scss")
+// styles for the markdown files that wont be purged because they are rendered after the purge process
+require("./src/styles/article-styles.scss")
 
-// require("prismjs/themes/prism-tomorrow.css")
+// turning off the default behaviour which will now be handled by 'gatsby-plugin-transitions'
+exports.shouldUpdateScroll = () => {
+  return false
+}
