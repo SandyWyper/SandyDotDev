13180cddf1d58d99da49a639af4f69050e6c5d9c
sandywyper <sandywyper@gmail.com>
2020-06-15 15:19:15 +0100

    Removed tranform from nav-title animations

--- src/components/navTitle.js
+++ src/components/navTitle.js
@@ -4,63 +4,57 @@ import { useSpring, animated } from "react-spring"
 import { Link } from "gatsby"
 
 const NavTitle = props => {
-  const [activeTitle, updateActiveTitle] = useState(null)
+  const [activeTitle, setActiveTitle] = useState(null)
 
   useEffect(() => {
-    updateActiveTitle(props.title)
+    setActiveTitle(props.title)
   }, [props.title])
 
   const projectSpring = useSpring({
     immediate: false,
     from: {
-      transform: `perspective(200px) rotateX(${
-        activeTitle !== "projects" ? 0 : 180
-      }deg)`,
+      // transform: `perspective(200px) rotateX(${
+      //   activeTitle !== "projects" ? 0 : 180
+      // }deg)`,
       opacity: activeTitle === "projects" ? 0 : 1,
-      // display: activeTitle === "projects" ? "none" : "block",
       marginTop: activeTitle === "projects" ? "-200px" : "0px",
     },
     opacity: activeTitle === "projects" ? 1 : 0,
-    // display: activeTitle === "projects" ? "block" : "none",
-    transform: `perspective(200px) rotateX(${
-      activeTitle !== "projects" ? 180 : 0
-    }deg)`,
+    // transform: `perspective(200px) rotateX(${
+    //   activeTitle !== "projects" ? 180 : 0
+    // }deg)`,
     marginTop: activeTitle !== "projects" ? "-200px" : "0px",
     config: { mass: 5, tension: 500, friction: 80 },
   })
   const blogSpring = useSpring({
     immediate: false,
     from: {
-      transform: `perspective(200px) rotateX(${
-        activeTitle !== "blog" ? 0 : 180
-      }deg)`,
+      // transform: `perspective(200px) rotateX(${
+      //   activeTitle !== "blog" ? 0 : 180
+      // }deg)`,
       opacity: activeTitle === "blog" ? 0 : 1,
-      // display: activeTitle === "blog" ? "none" : "block",
       marginTop: activeTitle === "blog" ? "-200px" : "0px",
     },
     opacity: activeTitle === "blog" ? 1 : 0,
-    // display: activeTitle === "blog" ? "block" : "none",
     marginTop: activeTitle !== "blog" ? "-200px" : "0px",
-    transform: `perspective(200px) rotateX(${
-      activeTitle !== "blog" ? 180 : 0
-    }deg)`,
+    // transform: `perspective(200px) rotateX(${
+    //   activeTitle !== "blog" ? 180 : 0
+    // }deg)`,
     config: { mass: 5, tension: 500, friction: 80 },
   })
   const contactSpring = useSpring({
     immediate: false,
     from: {
-      transform: `perspective(200px) rotateX(${
-        activeTitle !== "contact" ? 0 : 180
-      }deg)`,
+      // transform: `perspective(200px) rotateX(${
+      //   activeTitle !== "contact" ? 0 : 180
+      // }deg)`,
       opacity: activeTitle === "contact" ? 0 : 1,
-      // display: activeTitle === "contact" ? "none" : "block",
       marginTop: activeTitle === "contact" ? "-200px" : "0px",
     },
     opacity: activeTitle === "contact" ? 1 : 0,
-    // display: activeTitle === "contact" ? "block" : "none",
-    transform: `perspective(200px) rotateX(${
-      activeTitle !== "contact" ? 180 : 0
-    }deg)`,
+    // transform: `perspective(200px) rotateX(${
+    //   activeTitle !== "contact" ? 180 : 0
+    // }deg)`,
     marginTop: activeTitle !== "contact" ? "-200px" : "0px",
     config: { mass: 5, tension: 500, friction: 80 },
   })
