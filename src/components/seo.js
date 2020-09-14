14384a05691ed0b6c349c87ea65a59e4cf50e726
sandywyper <sandywyper@gmail.com>
2020-09-14 15:56:20 +0100

    Commented out the url for the moment

--- src/components/seo.js
+++ src/components/seo.js
@@ -48,7 +48,7 @@ function SEO({
     ? `${defaultUrl}${image}`
     : `${defaultUrl}${defaultImage}`
   const metaImageAlt = imageAlt || defaultTitle
-  const metaUrl = url ? url : defaultUrl
+  // const metaUrl = url ? url : defaultUrl
   const metaRobots = robots ? robots : "all"
 
   return (
@@ -65,7 +65,7 @@ function SEO({
         { property: `og:image`, content: metaImage },
         { name: "og:image:alt", content: metaImageAlt },
         { property: `og:type`, content: type ? type : `website` },
-        { property: `og:url`, content: metaUrl },
+        // { property: `og:url`, content: metaUrl },
         { name: `twitter:card`, content: `summary` },
         { name: `twitter:creator`, content: author },
         { name: `twitter:title`, content: title },
